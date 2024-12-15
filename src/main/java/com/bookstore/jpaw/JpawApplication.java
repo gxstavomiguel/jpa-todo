efcc63a7ec29c30413a63b5114c1f83d37c5f1a1
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-12-14 22:17:15 -0300

    Initial commit

---
+++ src/main/java/com/bookstore/jpaw/JpawApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.jpaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookstore/jpaw/JpawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.jpaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
