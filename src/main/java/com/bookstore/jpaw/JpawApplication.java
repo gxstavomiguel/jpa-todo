46a74491075db731615cc51c8c0d16d51d5f625d
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-12-15 20:06:11 -0300

    Entidades criadas

--- src/main/java/com/bookstore/jpaw/JpawApplication.java
+++ src/main/java/com/bookstore/jpaw/JpawApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpawApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpawApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpawApplication.class, args);
+    }
 
 }
